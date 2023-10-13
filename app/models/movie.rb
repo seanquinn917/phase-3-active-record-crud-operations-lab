3f46f86478e110d74176dcd76e96ec28cfa65e50
SeanQuinn <Seanquinn917@gmail.com>
2023-10-13 11:44:30 -0400

    commit tree 2337e861b636e0fbc85f3b0872060739229b335b

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,47 @@
 class Movie < ActiveRecord::Base
 
+
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.limit(1).first
+    end
+    
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count 
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.where(id: id).first
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        Movie.find_by(attributes)
+    end 
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > ?", 2002)
+    end
+
+    def update_with_attributes(attributes)
+        update(attributes)
+    end 
+
+    def self.update_all_titles(title)
+        Movie.update_all(title: title)
+    end
+
+    def self.delete_by_id(movie)
+        Movie.destroy(movie)
+    end
+
+    def self.delete_all_movies
+        Movie.delete_all
+    end
 end
